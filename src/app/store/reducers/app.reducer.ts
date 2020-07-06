0c9a640414f44ea4117b3fcdbe58fe6d6c37ead5
wojtfire <jakubekwojciech@gmail.com>
2020-07-06 10:14:27 +0200

    Auto-generated commit

--- src/app/store/reducers/app.reducer.ts
+++
@@ -1,9 +0,0 @@
-import {ActionReducerMap} from '@ngrx/store';
-import {AppState} from '../state/app.state';
-import {cartReducer} from './cart.reducer';
-import {creditCardReducer} from './credit-card.reducer';
-
-export const appReducers: ActionReducerMap<AppState, any> = {
-  cards: creditCardReducer,
-  cart: cartReducer
-};
--- src/app/store/selectors/cart.selectors.ts
+++
@@ -1,4 +0,0 @@
-import {CartItem, CartState} from '../state/cart.model';
-
-export const getCartOverallPrice = (state: CartState) =>
-  state.products.reduce((sum: number, product: CartItem) => product.product.price * product.quantity, 0);
